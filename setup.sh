3f585c1dee9fa1786d548bdef3d2ee3f6514ba61
aliraeisdanaei <ali.raeisdanaei@gmail.com>
2026-04-25 15:54:43 -0400

    Removed the stale setup scripts

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -sL install-node.vercel.app/lts | bash
-
-pip3 install neovim
-
-echo "Run PlugInstall"
--- setup2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Run with sudo"
-
-sudo npm install -g yarn
-cd ~/.vim/plugged/coc.nvim
-yarn install
-yarn build
